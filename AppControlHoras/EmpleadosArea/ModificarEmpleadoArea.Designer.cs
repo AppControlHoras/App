3b84519bb2ed8fe1725795ba8013dcf94457dae7
AlvaroCodice <alvaro.benito@codice-t.com>
2022-05-24 09:21:23 +0200

    ModificarEmpleadosArea terminado.

--- AppControlHoras/EmpleadosArea/ModificarEmpleadoArea.Designer.cs
+++ AppControlHoras/EmpleadosArea/ModificarEmpleadoArea.Designer.cs
@@ -28,12 +28,234 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ModificarEmpleadoArea));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btCambiar = new System.Windows.Forms.Button();
+            this.tbIdAreaNuevo = new System.Windows.Forms.TextBox();
+            this.lblNuevoTipo = new System.Windows.Forms.Label();
+            this.lblIdEmpleado2 = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblIdEmpleado = new System.Windows.Forms.Label();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.tbIdEmpleado = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblIdArea = new System.Windows.Forms.Label();
+            this.tbIdArea = new System.Windows.Forms.TextBox();
+            this.lblIdArea2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbIdEmpleadoNuevo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.tbIdEmpleadoNuevo);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.lblIdArea2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.btCambiar);
+            this.panel2.Controls.Add(this.tbIdAreaNuevo);
+            this.panel2.Controls.Add(this.lblNuevoTipo);
+            this.panel2.Controls.Add(this.lblIdEmpleado2);
+            this.panel2.Controls.Add(this.lblID);
+            this.panel2.Location = new System.Drawing.Point(53, 177);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(218, 131);
+            this.panel2.TabIndex = 10;
+            this.panel2.Visible = false;
+            // 
+            // btCambiar
+            // 
+            this.btCambiar.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btCambiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btCambiar.Location = new System.Drawing.Point(68, 105);
+            this.btCambiar.Name = "btCambiar";
+            this.btCambiar.Size = new System.Drawing.Size(75, 23);
+            this.btCambiar.TabIndex = 4;
+            this.btCambiar.Text = "Cambiar";
+            this.btCambiar.UseVisualStyleBackColor = true;
+            this.btCambiar.Click += new System.EventHandler(this.btCambiar_Click);
+            // 
+            // tbIdAreaNuevo
+            // 
+            this.tbIdAreaNuevo.Location = new System.Drawing.Point(95, 76);
+            this.tbIdAreaNuevo.Name = "tbIdAreaNuevo";
+            this.tbIdAreaNuevo.Size = new System.Drawing.Size(114, 20);
+            this.tbIdAreaNuevo.TabIndex = 4;
+            // 
+            // lblNuevoTipo
+            // 
+            this.lblNuevoTipo.AutoSize = true;
+            this.lblNuevoTipo.Location = new System.Drawing.Point(3, 79);
+            this.lblNuevoTipo.Name = "lblNuevoTipo";
+            this.lblNuevoTipo.Size = new System.Drawing.Size(67, 13);
+            this.lblNuevoTipo.TabIndex = 2;
+            this.lblNuevoTipo.Text = "Nuevo Area:";
+            // 
+            // lblIdEmpleado2
+            // 
+            this.lblIdEmpleado2.AutoSize = true;
+            this.lblIdEmpleado2.Location = new System.Drawing.Point(77, 14);
+            this.lblIdEmpleado2.Name = "lblIdEmpleado2";
+            this.lblIdEmpleado2.Size = new System.Drawing.Size(35, 13);
+            this.lblIdEmpleado2.TabIndex = 1;
+            this.lblIdEmpleado2.Text = "label1";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(3, 14);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(71, 13);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID Empleado:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblIdArea);
+            this.panel1.Controls.Add(this.tbIdArea);
+            this.panel1.Controls.Add(this.lblIdEmpleado);
+            this.panel1.Controls.Add(this.btBuscar);
+            this.panel1.Controls.Add(this.tbIdEmpleado);
+            this.panel1.Location = new System.Drawing.Point(53, 68);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(218, 103);
+            this.panel1.TabIndex = 9;
+            // 
+            // lblIdEmpleado
+            // 
+            this.lblIdEmpleado.AutoSize = true;
+            this.lblIdEmpleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdEmpleado.Location = new System.Drawing.Point(3, 9);
+            this.lblIdEmpleado.Name = "lblIdEmpleado";
+            this.lblIdEmpleado.Size = new System.Drawing.Size(71, 13);
+            this.lblIdEmpleado.TabIndex = 1;
+            this.lblIdEmpleado.Text = "ID Empleado:";
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btBuscar.Location = new System.Drawing.Point(68, 66);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btBuscar.TabIndex = 3;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // tbIdEmpleado
+            // 
+            this.tbIdEmpleado.Location = new System.Drawing.Point(101, 6);
+            this.tbIdEmpleado.Name = "tbIdEmpleado";
+            this.tbIdEmpleado.Size = new System.Drawing.Size(114, 20);
+            this.tbIdEmpleado.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::AppControlHoras.Properties.Resources.CodiceTitulo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblIdArea
+            // 
+            this.lblIdArea.AutoSize = true;
+            this.lblIdArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIdArea.Location = new System.Drawing.Point(3, 40);
+            this.lblIdArea.Name = "lblIdArea";
+            this.lblIdArea.Size = new System.Drawing.Size(46, 13);
+            this.lblIdArea.TabIndex = 4;
+            this.lblIdArea.Text = "ID Area:";
+            // 
+            // tbIdArea
+            // 
+            this.tbIdArea.Location = new System.Drawing.Point(101, 37);
+            this.tbIdArea.Name = "tbIdArea";
+            this.tbIdArea.Size = new System.Drawing.Size(114, 20);
+            this.tbIdArea.TabIndex = 5;
+            // 
+            // lblIdArea2
+            // 
+            this.lblIdArea2.AutoSize = true;
+            this.lblIdArea2.Location = new System.Drawing.Point(176, 14);
+            this.lblIdArea2.Name = "lblIdArea2";
+            this.lblIdArea2.Size = new System.Drawing.Size(35, 13);
+            this.lblIdArea2.TabIndex = 6;
+            this.lblIdArea2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ID Area:";
+            // 
+            // tbIdEmpleadoNuevo
+            // 
+            this.tbIdEmpleadoNuevo.Location = new System.Drawing.Point(95, 45);
+            this.tbIdEmpleadoNuevo.Name = "tbIdEmpleadoNuevo";
+            this.tbIdEmpleadoNuevo.Size = new System.Drawing.Size(114, 20);
+            this.tbIdEmpleadoNuevo.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nuevo Empleado:";
+            // 
+            // ModificarEmpleadoArea
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ModificarEmpleadoArea";
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(334, 311);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ModificarEmpleadoArea";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Modificar Empleado-Area";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox tbIdEmpleadoNuevo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblIdArea2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btCambiar;
+        private System.Windows.Forms.TextBox tbIdAreaNuevo;
+        private System.Windows.Forms.Label lblNuevoTipo;
+        private System.Windows.Forms.Label lblIdEmpleado2;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblIdArea;
+        private System.Windows.Forms.TextBox tbIdArea;
+        private System.Windows.Forms.Label lblIdEmpleado;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.TextBox tbIdEmpleado;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- AppControlHoras/EmpleadosArea/ModificarEmpleadoArea.cs
+++ AppControlHoras/EmpleadosArea/ModificarEmpleadoArea.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,58 @@ namespace AppControlHoras.EmpleadosArea
 {
     public partial class ModificarEmpleadoArea : Form
     {
+        private SqlConnection connection = new SqlConnection("Data Source = TERESA\\SERVERSQL; Initial Catalog = ControlHoras; Integrated Security = True");
         public ModificarEmpleadoArea()
         {
             InitializeComponent();
+        }
+
+        private void btBuscar_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            int idEmpleado = Convert.ToInt32(tbIdEmpleado.Text);
+            int idArea = Convert.ToInt32(tbIdArea.Text);
+
+            if(tbIdEmpleado.Equals("") || tbIdArea.Equals(""))
+            {
+                MessageBox.Show("Rellene los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Empleados_Area WHERE ID_EMPLEADO = '" + idEmpleado + "' AND ID_AREA = '"+ idArea +"'", connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    panel2.Visible = true;
+                    lblIdEmpleado2.Text = reader["ID_EMPLEADO"].ToString();
+                    lblIdArea2.Text = reader["ID_AREA"].ToString();
+                    tbIdEmpleadoNuevo.Text = reader["ID_EMPLEADO"].ToString();
+                    tbIdAreaNuevo.Text = reader["ID_AREA"].ToString();
+                }
+            }
+            connection.Close();
+        }
+
+        private void btCambiar_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            int idEmpleadoNuevo = Convert.ToInt32(tbIdEmpleadoNuevo.Text);
+            int idAreaNuevo = Convert.ToInt32(tbIdAreaNuevo.Text);
+            int idEmpleado = Convert.ToInt32(lblIdEmpleado2.Text);
+            int idArea = Convert.ToInt32(lblIdArea2.Text);
+
+            if (idAreaNuevo.Equals("") || idEmpleadoNuevo.Equals(""))
+            {
+                MessageBox.Show("Debes rellenar todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand("UPDATE Empleados_Area SET ID_EMPLEADO = '" + idEmpleadoNuevo + "', ID_AREA = '"+ idAreaNuevo +"' WHERE ID_EMPLEADO = '" + idEmpleado + "' AND ID_AREA = '"+ idArea +"'", connection);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Empleado-Area modificado correctamente", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            connection.Close();
+            this.Close();
         }
     }
 }
